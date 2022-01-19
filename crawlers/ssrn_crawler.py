ebcf2f7a80419f6846b5565622edfe74991ad7b3
Yassine <keptsa@yahoo.fr>
2022-01-19 20:15:02 +0100

    crawlers correction

--- crawlers/ssrn_crawler.py
+++ crawlers/ssrn_crawler.py
@@ -2,6 +2,8 @@ import re
 from tqdm.notebook import tqdm
 from bs4 import BeautifulSoup
 import requests
+import time
+import random
 
 def select_span(list_spans):
     for elt in list_spans:
@@ -15,14 +17,26 @@ class SSRN_Crawler:
         self.path = path
         self.soup = {}
         self.total_pages = 999
-        self.docs = {'title': [], 'abstract_id': [], 'authors': [], 'date': [], 'field': [], 'nb_page': []}
+        self.docs = {'title': [], 'abstract_id': [], 'authors': [], 'date': [], 'field': [], 'nb_page': [],"abstract_box":[]}
 
     def get_soup(self, path):
         header = {}
         header[
             'User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
         soup = BeautifulSoup(requests.get(path, headers=header, timeout=10).content, 'html.parser')
+        slp=random.randint(2,5)
+        time.sleep(slp)
         return soup
+
+    def get_abstract_box(self,id):
+        abstract_path = "https://papers.ssrn.com/sol3/papers.cfm?abstract_id="+id
+        abstract_soup = self.get_soup(abstract_path)
+        abstract_box = abstract_soup.find_all(name="div", attrs={"class": "box-container box-abstract-main"})
+        if not abstract_box:
+          print('soup :  ',abstract_soup)
+          print('problem with abstract id : ', id)
+          print('abst box : ', abstract_box)
+        return abstract_box[0]
 
     def get_page_path(self, nb_page):
         split = self.path.split('cfm?')
@@ -44,15 +58,21 @@ class SSRN_Crawler:
         for des in descriptions:
             title = self.get_title(des)
             if title:
-                self.docs['title'].append(title)
-                self.docs['abstract_id'].append(self.get_abstract_id(des))
-                self.docs['authors'].append(self.get_authors(des))
-                self.docs['date'].append(self.get_date(des))
-                self.docs['field'].append(self.field)
-                self.docs['nb_page'].append(npage)
+                id =  self.get_abstract_id(des)
+                if id not in self.docs['abstract_id'] and title not in self.docs['title']:
+                  self.docs['title'].append(title)
+                  self.docs['abstract_id'].append(id)
+                  self.docs['abstract_box'].append(self.get_abstract_box(id))
+                  self.docs['authors'].append(self.get_authors(des))
+                  self.docs['date'].append(self.get_date(des))
+                  self.docs['field'].append(self.field)
+                  self.docs['nb_page'].append(npage)
 
     def get_title(self, description_soup):
-        return description_soup.find_all(name="a", attrs={"class": "title optClickTitle"})[0].string
+        title= description_soup.find_all(name="a", attrs={"class": "title optClickTitle"})[0].string
+        if not title:
+            title = description_soup.find_all(name="a", attrs={"class": "title optClickTitle"})[0].find_all(name="span")[0].string
+        return title
 
     def get_abstract_id(self, description_soup):
         return description_soup.find_all(name="a", attrs={"class": "title optClickTitle"})[0]['href'].split('=')[1]
@@ -71,7 +91,9 @@ class SSRN_Crawler:
 
     def get_docs(self):
         if not self.soup:
+            print('Getting soups..')
             self.get_all_soups()
-        for page in range(1, self.total_pages + 1):
+        print('Getting description..')
+        for page in tqdm(range(1, self.total_pages + 1)):
             self.get_description_data_npage(page)
 
