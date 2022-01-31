64ba3def69cb46a74a3233813b1332aa1268508a
Yassine <keptsa@yahoo.fr>
2022-01-31 21:20:35 +0100

    small corr

--- crawlers/ssrn_crawler.py
+++ crawlers/ssrn_crawler.py
@@ -30,7 +30,7 @@ class SSRN_Crawler:
         header = {}
         header[
             'User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
-        soup = BeautifulSoup(requests.get(path, headers=header, timeout=10).content, 'html.parser')
+        soup = BeautifulSoup(requests.get(path, headers=header, timeout=15).content, 'html.parser')
         slp=random.randint(self.t_min,self.t_max)
         time.sleep(slp)
         return soup
