38a63d8328da44f8836604c6c130b23891a885ae
Yassine <keptsa@yahoo.fr>
2022-03-15 17:50:44 +0100

    small modif

--- data_constructors/omr_classif_data.py
+++ data_constructors/omr_classif_data.py
@@ -168,7 +168,7 @@ class OMRData:
             dict_data = self.BOMR2dict_data(bomr,doi)
             df = pd.DataFrame(dict_data)
             list_bomr_dfs.append(df)
-        dfs_concatenated = pd.concat(list_bomr_dfs)
+        dfs_concatenated = pd.concat(list_bomr_dfs, ignore_index=True)
         dfs_concatenated['start']= dfs_concatenated['start'].astype(int)
         dfs_concatenated['end']= dfs_concatenated['end'].astype(int)
         dfs_concatenated['class_num']= dfs_concatenated['class_num'].astype(int)
