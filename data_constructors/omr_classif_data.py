85e94faaa8106b08dbad5fbf5c752b9ea4ea7991
Yassine <keptsa@yahoo.fr>
2022-03-17 15:17:58 +0100

    modified omr data for ner

--- data_constructors/omr_classif_data.py
+++ data_constructors/omr_classif_data.py
@@ -2,15 +2,31 @@ import numpy as np
 import pandas as pd
 import ast
 import re
+import random
+from tqdm.notebook import tqdm
+
+tqdm.pandas()
 
 from naimai.constants.regex import regex_background, regex_objective,regex_methods,regex_results, regex_objectives
+from naimai.papers.full_text.pmc import paper_pmc
+
+def entity_annotate(text, label):
+    split = text.split()
+    if label=='other':
+        entities_labels = ['O' for _ in split]
+    else:
+        entities_labels = ['B-{}'.format(label)]
+        for _ in split[1:]:
+            entities_labels.append('I-{}'.format(label))
+    return entities_labels
 
 class OMRData:
     '''
       Construct data for OMR classification, based on pmc dataframes :
           1. transform structured abstracts to the BOMR structure {Background:.., Objective:.., Methods:.., Results:..}
           2. move objective phrases in background (if exist) to objective section in BOMR
-          3. convert BOMR dict to df
+          3. add Other phrases from the body (that would be classified as O in NER labelling), we then get : BOMRO (BOMR + 'Other' section)
+          3. convert BOMRO dict to df
           4. Stack & Format the data in the input format for the classifiers
       '''
     def __init__(self, data_df=None,path_data=''):
@@ -19,9 +35,11 @@ class OMRData:
         else:
             data = data_df
         data['is_structured'] = data['abstract'].apply(self.is_structured)
-        self.data = data[['doi', 'abstract']][data['is_structured'] == True] # only structured abstracts & dois are selected
+        self.data = data[['doi', 'abstract','body']][data['is_structured'] == True] # only structured abstracts & dois & body are selected
+        self.data = self.data.reset_index(drop=True)
         self.transformed_data = None
-        self.BOMR_class_num = {'b':0,'o':1,'m':2,'r':3}
+        self.NER_data = None
+        self.BOMRO_class_num = {'ba':0,'ob':1,'me':2,'re':3,'ot':4}
 
     def is_structured(self,text):
         '''
@@ -85,11 +103,13 @@ class OMRData:
                 for obj in obj_phrases:
                     BOMR['objectives']+= obj
                     BOMR['background']=BOMR['background'].replace(obj,'')
+        BOMR['objectives'] = BOMR['objectives'].strip()
+        BOMR['background'] = BOMR['background'].strip()
         return BOMR
 
-    def abstract2BOMR(self,abstract_dict):
+    def abstract2BOMRO(self, abstract_dict):
         '''
-        map abstract dict to BOMR dict
+        map abstract dict to BOMRO dict
         :param abstract_dict:
         :return:
         '''
@@ -104,29 +124,49 @@ class OMRData:
                 BOMR[head] +=' '.join(abstract_dict[header])
             BOMR[head]=BOMR[head].strip()
         BOMR_corrected = self.correct_BOMR(BOMR) #move obj phrases from background to objective
+
+        idx_df = self.data.index[self.data['abstract']==str(abstract_dict)].to_list()[0]
+        other_sentences = self.get_O_entity_label(idx=idx_df)
+        if other_sentences:
+            BOMR['other'] = [elt.strip() for elt in other_sentences]
+            BOMR['other'] = '. '.join(BOMR['other'])
+            BOMR['other']=BOMR['other'].strip()
+        else:
+            BOMR['other']=''
+
         return BOMR_corrected
+
     def get_headers_order(self,order='BOMR'):
         '''
         give list of BOMR headers following one of 3 orders :
             1. BOMR
             2. OBMR
             3. MOBR
+        the Other section is randomply placed either at the beginning or the end.
         :param order:
         :return: list of headers
         '''
         if order=='BOMR':
-            return ['background', 'objectives', 'methods', 'results']
+            result= ['background', 'objectives', 'methods', 'results']
         elif order=='OBMR':
-            return ['objectives','background','methods','results']
+            result= ['objectives','background','methods','results']
         elif order=='MOBR':
-            return ['methods','objectives','background','results']
+            result= ['methods','objectives','background','results']
         else:
             print('not well written.., returned the default BOMR')
-            return ['background', 'objectives', 'methods', 'results']
+            result= ['background', 'objectives', 'methods', 'results']
+
+        condition=random.choice([0,1])
+        if condition:
+            result = ['other'] + result
+        else:
+            result += ['other']
+        return result
 
     def BOMR2dict_data(self,BOMR,id_BOMR,order='BOMR'):
         '''
-        transforms BOMR dictionary into data disctionary following the given order. Default order : BOMR
+        transforms BOMR dictionary into data disctionary following the given order. Default order : BOMR. if add_other,
+        get other sentences that are not BOMR
         :param BOMR: BOMR dictionary
         :param id_BOMR: id of the BOMR
         :param order: string : BOMR, OBMR or MOBR
@@ -147,18 +187,19 @@ class OMRData:
                 words_ids = list(np.arange(len(text.split()))+last_word_id+1)
                 dict_data['predict_string'].append(words_ids)
                 dict_data['class'].append(head)
-                first_class_letter = head[0]
-                dict_data['class_num'].append(self.BOMR_class_num[first_class_letter])
+                first_class_letter = head[:2]
+                dict_data['class_num'].append(self.BOMRO_class_num[first_class_letter])
 
                 id_str_start+=len(BOMR[head])
                 last_word_id=words_ids[-1]
 
         dict_data['id'] = [id_BOMR]*len(dict_data['text'])
+
         return dict_data
 
     def BOMR2text(self,BOMR,order='BOMR'):
         '''
-        transforms BOMR dictionary into plain text following the given order. Default order : BOMR
+        transforms BOMR dictionary into plain text following the given order. Default order : BOMR (with other at the beginning or end)
         :param BOMR:
         :param order:
         :return:
@@ -170,13 +211,24 @@ class OMRData:
 
         return text.strip()
 
+    def BOMR2entities(self,BOMR,order='BOMR'):
+        '''
+        transforms BOMR dictionary into entities labels : B-label, I-label...
+        :param BOMR:
+        :return:
+        '''
+        headers = self.get_headers_order(order=order)
+        entities = {}
+        for head in headers:
+            entities[head] = entity_annotate(text=BOMR[head],label=head)
+        return entities
 
     def data2BOMR_df(self):
         '''
-        convert the data df to BOMR dataframe, which is the input to models..
+        convert the data df to BOMR dataframe, with starts, end, etc for each sentence
         :return:
         '''
-        list_BOMR_dicts = list(self.data['abstract'].apply(self.abstract2BOMR))
+        list_BOMR_dicts = list(self.data['abstract'].progress_apply(self.abstract2BOMRO))
         list_dois = list(self.data['doi'])
         list_bomr_dfs = []
         for bomr,doi in zip(list_BOMR_dicts,list_dois):
@@ -188,3 +240,64 @@ class OMRData:
         dfs_concatenated['end']= dfs_concatenated['end'].astype(int)
         dfs_concatenated['class_num']= dfs_concatenated['class_num'].astype(int)
         self.transformed_data= dfs_concatenated
+
+    def data2BOMR_NER_df(self):
+        '''
+        convert the data df to BOMR dataframe only with doi, text and entities. This could be the input to NER model
+        :return:
+        '''
+        list_BOMR_dicts = list(self.data['abstract'].progress_apply(self.abstract2BOMRO))
+        list_dois = list(self.data['doi'])
+        abstract_list = []
+        entities_list = []
+        order_list=[]
+        orders = ['BOMR', 'OBMR', 'MOBR']
+        for bomr,doi in zip(list_BOMR_dicts,list_dois):
+            order=random.choices(orders, weights=(60, 20, 20), k=1)[0]
+            abstract_list.append(self.BOMR2text(bomr,order=order))
+            entities=self.BOMR2entities(bomr,order=order)
+            entities_vals = []
+            for head in entities:
+                entities_vals += entities[head]
+            entities_list.append(entities_vals)
+            order_list.append(order)
+        self.NER_data = pd.DataFrame({'doi': list_dois,
+                           'text': abstract_list,
+                           'entities': entities_list,
+                           'order_list': order_list})
+
+
+
+
+
+    def get_O_entity_from_text(self,text):
+        '''
+        get labeled O entities from text
+        :param text:
+        :return:
+        '''
+        split = text.split('.')
+        split_no_obj = [elt for elt in split if not re.findall(regex_objectives,elt,flags=re.I)]
+        return random.choice(split_no_obj)
+
+    def get_O_entity_label(self,doi=None,idx=None):
+        '''
+        get labeled O entities from body. Should get either doi or idx
+        :param doi:
+        :param idx:
+        :return:
+        '''
+        O_labelled_list=[]
+        if doi:
+            idx=self.data.index[self.data['doi']==doi].to_list()[0]
+        else:
+            idx=idx
+        paper = paper_pmc(df=self.data,idx_in_df=idx)
+        paper.get_content()
+        if paper.unclassified_section:
+            for head in paper.unclassified_section:
+                text=paper.unclassified_section[head]
+                O_labelled_list.append(self.get_O_entity_from_text(text))
+            return list(set(O_labelled_list))
+        else:
+            return []
