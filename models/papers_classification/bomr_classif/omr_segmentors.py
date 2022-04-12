706f85273a860f0d1264fcf20e16c840469290f5
Yassine <keptsa@yahoo.fr>
2022-04-12 17:31:22 +0200

    omr_text_segmentor implementation

---
+++ models/papers_classification/bomr_classif/omr_segmentors.py
@@ -0,0 +1,76 @@
+from naimai.constants.regex import regex_sentences, regex_filtered_words_obj
+from naimai.constants.paths import path_objective_classifier
+from naimai.utils.regex import get_first_last_token_ids
+from naimai.models.papers_classification.bomr_classif.ner_processor import NER_BOMR_processor
+from naimai.models.papers_classification.bomr_classif.ner_classifier import NER_BOMR_classifier
+from naimai.models.papers_classification.obj_classifier import Objective_classifier
+import re
+
+
+class OMR_Text_Segmentor:
+    def __init__(self, text, bomr_classifier=None, objective_classifier=None, path_model=''):
+        self.text = text
+        self.ner_processor = None
+        self.sentences = re.split(regex_sentences, text)
+        if bomr_classifier:
+            self.bomr_classifier = bomr_classifier
+        elif path_model:
+            self.bomr_classifier = NER_BOMR_classifier(load_model=True, path_model=path_model, predict_mode=True)
+        print('Predicting & getting ner processor..')
+        self.get_ner_processor()
+        if objective_classifier:
+          self.objective_classifier = objective_classifier
+        else:
+          print('Getting objectif classifier..')
+          self.objective_classifier = Objective_classifier(dir=path_objective_classifier)
+
+        print('Done!')
+    def text2bomr(self, text, visualize_=False):
+        return self.bomr_classifier.predict(text=text, visualize_=visualize_, dict_format=False)
+
+    def get_ner_processor(self):
+        df = self.text2bomr(self.text)
+        self.ner_processor = NER_BOMR_processor(text=self.text, df=df)
+
+    def get_sentence_label(self, sentence, last_token=0):
+        sentence_range_wids = get_first_last_token_ids(sentence, self.text, last_token)
+        range_wids_labels = self.ner_processor.get_range_wids_labels()
+        sentence_class = self.ner_processor.get_label_with_most_overlap(sentence_range_wids=sentence_range_wids,
+                                                                        range_wids_labels=range_wids_labels)
+        return (sentence_class, sentence_range_wids[-1] + 1)
+
+    def segment_naive(self) -> dict:
+        '''
+        segment text into bomr using longformer predictions & ner processor
+        :return:
+        '''
+        last_tokenId = 0
+        segmented_text = {}
+
+        for idx, sentence in enumerate(self.sentences):
+            segmented_text[idx], last_tokenId = self.get_sentence_label(sentence, last_token=last_tokenId)
+        return segmented_text
+
+    def segment(self) -> dict:
+        '''
+        segment naively the text & process each background & objective classes : get matched objective sentences with objective classifier
+        :return:
+        '''
+        segmented_text = self.segment_naive()
+        regex_objective_bground = 'background|objectives'
+        objective_bground_sentences = [self.sentences[idx] for idx in segmented_text if
+                                       re.findall(regex_objective_bground, segmented_text[idx])]
+        objectives = self.objective_classifier.predict(objective_bground_sentences)
+        objectives_with_classifier = [obj for obj in objectives if
+                                      not re.findall(regex_filtered_words_obj, obj, flags=re.I)]
+        obj_sentences_ids = [self.sentences.index(elt) for elt in objectives_with_classifier]
+
+        corrected = {}
+        for idx in obj_sentences_ids:
+            corrected[idx] = 'objectives'
+
+        for idx in segmented_text:
+            if not re.findall(regex_objective_bground, segmented_text[idx]):
+                corrected[idx] = segmented_text[idx]
+        return corrected
+
