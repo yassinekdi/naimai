118fd809f2e95b3dd384d71e0d636b06610743a0
Yassine <keptsa@yahoo.fr>
2022-04-12 09:42:59 +0200

    find highest overlap

--- models/papers_classification/bomr_classif/ner_processor.py
+++ models/papers_classification/bomr_classif/ner_processor.py
@@ -51,3 +51,51 @@ class NER_BOMR_processor:
         self.df['start_char'] = self.df.apply(get_first_char_id, args=(self.text,), axis=1)
         self.df['last_char'] = self.df.apply(get_last_char_id, args=(self.text,), axis=1)
         self.df = self.df.sort_values(by=['start']).reset_index(drop=True)
+
+    def len_intersection_2_ranges(self,rang1: range, rang2: range) -> int:
+        '''
+        get length of intersection between range1 and range2
+        :param rang1: range
+        :param rang2:
+        :return:
+        '''
+        xs = set(rang1)
+        return len(xs.intersection(rang2))
+
+    def intersection_with_class(self,sentence_range: range, class_ranges: range) -> int:
+        '''
+        get length of intersection between range of wids on of sentence_range and list of ranges in class ranges
+        :param sentence_range:
+        :param class_ranges:
+        :return:
+        '''
+        sum = 0
+        for rg in class_ranges:
+            sum += self.len_intersection_2_ranges(rg, sentence_range)
+        return sum
+
+    def get_range_wids_labels(self) -> dict:
+        '''
+        get, for each label, list of ranges between start word ids and last word ids
+        :return:
+        '''
+        labels = self.df['class'].unique().tolist()
+        range_wids_labels = {elt: [] for elt in labels}
+        for _, elt in self.df.iterrows():
+            class_ = elt['class']
+            start, end = elt['start'], elt['end']
+            range_wids_labels[class_].append(range(start, end))
+        return range_wids_labels
+
+    def get_label_with_most_overlap(self,sentence_range_wids: range, range_wids_labels: dict) -> str:
+        '''
+        find label with most overlap : with the higher intersection between class ranges and sentence range of wids.
+        :return:
+        '''
+        overlaps = {}
+        for cls in range_wids_labels:
+            class_ranges = range_wids_labels[cls]
+            overlaps[cls]= self.intersection_with_class(sentence_range_wids,class_ranges)
+        highest_overlap = max(overlaps.values())
+        highest_class = [elt for elt in overlaps if overlaps[elt]==highest_overlap]
+        return highest_class[0]
