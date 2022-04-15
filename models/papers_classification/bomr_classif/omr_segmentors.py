d339b6b18f5d0f964cfd21fae7c4da1b7d2a4c41
Yassine <keptsa@yahoo.fr>
2022-04-15 15:32:56 +0200

    some modifs for segmentors

--- models/papers_classification/bomr_classif/omr_segmentors.py
+++ models/papers_classification/bomr_classif/omr_segmentors.py
@@ -16,27 +16,33 @@ class OMR_Text_Segmentor:
     def __init__(self, text, bomr_classifier=None, objective_classifier=None, path_model='',nlp=None,summarize=False):
         self.text = text
         self.ner_processor = None
+        self.range_wids_labels=None
+
+        # Segment to sentences
+        if not nlp:
+            nlp = spacy.load(nlp_vocab)
+        doc = nlp(text)
+        self.sentences = [sent.text.strip() for sent in doc.sents if len(sent.text.split())>5]
+        self.text = ' '.join(self.sentences)
+
+        # Get obj classifier
+        if objective_classifier:
+          self.objective_classifier = objective_classifier
+        else:
+          print('Getting objectif classifier..')
+          self.objective_classifier = Objective_classifier(dir=path_objective_classifier)
+
+        if summarize:
+            self.summarize()
+        # Get bomr classifier
         if bomr_classifier:
             self.bomr_classifier = bomr_classifier
         elif path_model:
             self.bomr_classifier = NER_BOMR_classifier(load_model=True, path_model=path_model, predict_mode=True)
         print('Predicting & getting ner processor..')
         self.get_ner_processor()
-        if objective_classifier:
-          self.objective_classifier = objective_classifier
-        else:
-          print('Getting objectif classifier..')
-          self.objective_classifier = Objective_classifier(dir=path_objective_classifier)
-        if nlp:
-            self.nlp = nlp
-        else:
-            self.nlp = spacy.load(nlp_vocab)
-        doc = self.nlp(text)
-        self.sentences = [sent.text.strip() for sent in doc.sents if len(sent.text.split())>5]
-        if summarize:
-            self.summarize()
-
         print('Done!')
+
     def text2bomr(self, text, visualize_=False):
         return self.bomr_classifier.predict(text=text, visualize_=visualize_, dict_format=False)
 
@@ -46,9 +52,10 @@ class OMR_Text_Segmentor:
 
     def get_sentence_label(self, sentence, last_token=0):
         sentence_range_wids = get_first_last_token_ids(sentence, self.text, last_token)
-        range_wids_labels = self.ner_processor.get_range_wids_labels()
+        if not self.range_wids_labels:
+            self.range_wids_labels = self.ner_processor.get_range_wids_labels()
         sentence_class = self.ner_processor.get_label_with_most_overlap(sentence_range_wids=sentence_range_wids,
-                                                                        range_wids_labels=range_wids_labels)
+                                                                        range_wids_labels=self.range_wids_labels)
         return (sentence_class, sentence_range_wids[-1] + 1)
 
     def segment_naive(self) -> dict:
@@ -79,25 +86,28 @@ class OMR_Text_Segmentor:
                                       not re.findall(regex_filtered_words_obj, obj, flags=re.I)]
         return objectives_with_classifier
 
-    def segment(self) -> dict:
+    def segment(self,check_background=False) -> dict:
         '''
-        segment naively the text & process each background classes : get matched objective sentences with objective classifier.
+        segment naively the text & if check_background, process each background classes : get matched objective sentences with objective classifier.
+        :param check_background:
         :return:
         '''
         segmented_text = self.segment_naive()
-        regex_bground = 'background'
-        bground_sentences = [self.sentences[idx] for idx in segmented_text if
-                                       re.findall(regex_bground, segmented_text[idx])]
+        if check_background:
+            regex_bground = 'background'
+            bground_sentences = [self.sentences[idx] for idx in segmented_text if
+                                           re.findall(regex_bground, segmented_text[idx])]
 
-        objectives_with_classifier = self.get_objective_sentences(bground_sentences)
-        obj_sentences_ids = [self.sentences.index(elt) for elt in objectives_with_classifier]
+            objectives_with_classifier = self.get_objective_sentences(bground_sentences)
+            obj_sentences_ids = [self.sentences.index(elt) for elt in objectives_with_classifier]
 
-        corrected = {}
-        for idx in obj_sentences_ids:
-            corrected[idx] = 'objectives'
+            corrected = {}
+            for idx in obj_sentences_ids:
+                corrected[idx] = 'objectives'
 
-        for idx in segmented_text:
-            if not re.findall(regex_bground, segmented_text[idx]):
-                corrected[idx] = segmented_text[idx]
-        return corrected
+            for idx in segmented_text:
+                if not re.findall(regex_bground, segmented_text[idx]):
+                    corrected[idx] = segmented_text[idx]
+            return corrected
+        return segmented_text
 
