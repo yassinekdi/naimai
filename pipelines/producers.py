58e4368131d4526c86b1fdd457fefd5f347a2e8c
Yassine <keptsa@yahoo.fr>
2022-05-07 12:14:29 +0200

    small corr

--- pipelines/producers.py
+++ pipelines/producers.py
@@ -263,7 +263,7 @@ class Field_Producer:
     def get_field_index(self):
         print('>> Computing Faiss Index..')
         fnames = list(self.production_field.keys())
-        to_encode = [self.txt_to_encode[fn] for fn in fnames]
+        to_encode = [self.txt_to_encode(fn) for fn in fnames]
         encoded_fields = self.encoder.encode(to_encode)
         encoded_fields = np.asarray(encoded_fields.astype('float32'))
         self.field_index = faiss.IndexIDMap(faiss.IndexFlatIP(768))
