734cd5b7a3d0a94887d140b72dd13dea5bf927d5
Yassine <keptsa@yahoo.fr>
2022-06-14 08:07:16 +0200

    querier first update

--- pipelines/querier.py
+++ pipelines/querier.py
@@ -1,5 +1,5 @@
 from naimai.constants.paths import path_produced
-from naimai.utils.general import load_gzip
+from naimai.utils.general import load_and_combine
 from sentence_transformers import SentenceTransformer
 import numpy as np
 import faiss
@@ -12,6 +12,15 @@ class Querier:
     self.field_index=field_index
     self.papers= papers
     self.fname = fname
+    if not encoder:
+      print('>> Loading encoder..')
+      self.load_encoder()
+    if not papers:
+      print('>> Loading papers..')
+      self.load_papers()
+    if not field_index:
+      print('>> Loading field index..')
+      self.load_field_index()
 
   def load_field_index(self):
     if not self.field_index:
@@ -21,28 +30,28 @@ class Querier:
   def load_papers(self):
     if not self.papers:
       path = os.path.join(path_produced,self.field,self.fname)
-      self.papers = load_gzip(path)
+      self.papers = load_and_combine(path)
 
   def load_encoder(self):
     if not self.encoder:
       path = os.path.join(path_produced, self.field, 'search_model')
       self.encoder = SentenceTransformer(path)
 
-  def review(self,query, top_n=4,text=True):
-
-    self.load_encoder()
-    self.load_papers()
-    self.load_field_index()
-
+  def get_similar_papers_fnames(self,query,top_n=5):
     encoded_query = self.encoder.encode([query])
-    top_n = self.field_index.search(encoded_query,top_n)
+    top_n = self.field_index.search(encoded_query, top_n)
     ids = top_n[1].tolist()[0]
     distances = top_n[0].tolist()[0]
     fnames = list(self.papers.keys())
-    similar_papers_fnames= list(np.unique(fnames)[ids])
-    reported_texts = [self.papers[fn]['reported'] for fn in similar_papers_fnames]
-    if text:
-      return ' '.join(reported_texts)
-    else:
-      results = [(dist, report) for dist, report in zip(distances, reported_texts)]
-      return results
+    similar_papers_fnames = list(np.unique(fnames)[ids])
+    return (similar_papers_fnames,distances)
+
+  def review(self,query, top_n=4,text=True):
+    pass
+
+    # reported_texts = [self.papers[fn]['reported'] for fn in similar_papers_fnames]
+    # if text:
+    #   return ' '.join(reported_texts)
+    # else:
+    #   results = [(dist, report) for dist, report in zip(distances, reported_texts)]
+    #   return results
