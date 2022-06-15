66fee47385e3b9b2bc4a433be5082caec4659eb6
Yassine <keptsa@yahoo.fr>
2022-06-15 18:22:41 +0200

    langage verification

--- papers/only_abstracts/hal.py
+++ papers/only_abstracts/hal.py
@@ -69,17 +69,18 @@ class papers_hal(papers):
                                     idx_in_df=idx_in_data)
             new_paper.get_doi()
             if not new_paper.is_in_database(self.naimai_dois):
-                new_paper.get_Abstract()
-                if len(new_paper.Abstract.split()) > 5:
-                    new_paper.get_fields()
-                    new_paper.get_Title()
-                    new_paper.get_journal()
-                    new_paper.get_Authors()
-                    new_paper.get_year()
-                    new_paper.replace_abbreviations()
-                    new_paper.get_numCitedBy()
-                    self.elements[new_paper.doi] = new_paper.save_dict()
-                    self.naimai_dois.append(new_paper.doi)
+                new_paper.get_Title()
+                if new_paper.is_paper_english(self.nlp):
+                    new_paper.get_Abstract()
+                    if len(new_paper.Abstract.split()) > 5:
+                        new_paper.get_fields()
+                        new_paper.get_journal()
+                        new_paper.get_Authors()
+                        new_paper.get_year()
+                        new_paper.replace_abbreviations()
+                        new_paper.get_numCitedBy()
+                        self.elements[new_paper.doi] = new_paper.save_dict()
+                        self.naimai_dois.append(new_paper.doi)
 
 
     @update_naimai_dois
