921f4074b25b7880788cf97fc635610cd6cd5c51
Yassine <keptsa@yahoo.fr>
2022-12-05 20:47:19 +0100

    rewriting setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+
+setup(
+    name = 'naimai',
+    version = '1.0.0',
+    author=  'Yassine Kaddi',
+    author_email = 'yassine@naimai.fr',
+    description = 'Python library to help with scientific literature research',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    maintainer = "Yassine Kaddi",
+    maintainer_email = "yassine@naimai.fr",
+    license = "CC BY-NC-SA",
+    keywords = ['science', 'review','bibliography','python','nlp','machine-learning','information-extraction'],
+    url =" https://github.com/yassinekdi/naimai",
+    classifiers =[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent"
+    ],
+    package_dir = {'': 'naimai'},
+    packages = find_packages()
+)
