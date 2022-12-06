603aa326847e004c1b14015f96608d8c78774c8e
Yassine <keptsa@yahoo.fr>
2022-12-06 16:55:46 +0100

    adding init files

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name = 'naimai',
-    version = '1.0.0',
+    version = '1.0.0.2',
     author=  'Yassine Kaddi',
     author_email = 'yassine@naimai.fr',
     description = 'Python library to help with scientific literature research',
